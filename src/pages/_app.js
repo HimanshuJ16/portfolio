bffc83655a87152be6a66400bccc84d81afc4689
Himanshu Jangir <hjuberoi16@gmail.com>
2023-11-12 18:34:34 +0530

    latest commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/11.jpg" />
+        <link rel="shortcut icon" href="/11.jpg" type="image/x-icon" /> 
       </Head>
       <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
         <NavBar />
