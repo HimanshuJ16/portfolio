bc4c4827dcf6f92ea5961192cd73342625cd61f2
Himanshu Jangir <hjuberoi16@gmail.com>
2023-11-15 01:38:57 +0530

    latest commit

---
+++ src/components/Cursor.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+const Cursor = () => {
+  const [cursorStyle, setCursorStyle] = useState({
+    left: 0,
+    top: 0,
+  });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setCursorStyle({
+        left: event.clientX,
+        top: event.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <>
+    <div>
+    <div
+      className="h-[60px] w-[30px] bg-primary dark:bg-primaryDark fixed rounded-full z-[9] blur-[50px] md:h-0 xl:h-0"
+      style={{
+        left: cursorStyle.left,
+        top: cursorStyle.top,
+      }}
+    />
+    <div
+      className="cursor h-[15px] w-[15px] bg-primary dark:bg-primaryDark fixed rounded-full z-99 md:h-0 xl:h-0"
+      style={{
+        left: cursorStyle.left,
+        top: cursorStyle.top,
+      }}
+    />
+    </div>
+
+
+    </>
+  );
+};
+
+export default Cursor;
