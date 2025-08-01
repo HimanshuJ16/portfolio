bdba7535f8915ef9cbbc8ab48aab22c2babda5e7
unknown <himanshujangir16@gmail.com>
2025-08-01 15:08:07 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
-                <Link href="himanshu resume.pdf" target={'_blank'}
+                <Link href="https://drive.google.com/file/d/1qH10khSlMl0Rx2WEX5bQb0HWbyShHRgg/view?usp=sharing" target={'_blank'}
                 className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:text-light hover:dark:bg-dark hover:dark:border-light md:p-2 md:px-4 md:text-base"
                 download={true}
                 >Resume<LinkArrow className={"w-6 ml-1"}/>
